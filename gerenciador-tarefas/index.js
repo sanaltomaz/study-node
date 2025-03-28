26bcae5f40b297047dd4604c4ff6bab77fc0e8d5
Sanal T R da Silva <tomazsanal@gmail.com>
2025-03-28 12:53:24 -0300

    Gerenciador de Tarefas: criando um menu interativo no terminal.

--- gerenciador-tarefas/index.js
+++ gerenciador-tarefas/index.js
@@ -1,26 +1,76 @@
+import readline from 'readline';
 import GerenciadorTarefas from "./GerenciadorTarefas.js";
 import Tarefa from "./Tarefa.js";
 
-const dados = "./dados.json";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+const dados = "./dados.json";
 const gerenciador = new GerenciadorTarefas(dados);
 
-// gerenciador.adicionarTarefa(new Tarefa("Fazer exercícios"))
+function menu() {
+    console.log(`
+        Gerenciador de Tarefas:
+        1 - Adicionar tarefa
+        2 - Remover tarefa
+        3 - Listar tarefas 
+        4 - Alterar status
+        5 - Filtrar tarefas
+        0 - Sair
+        `);
+    
+        rl.question('Escolha uma opção: ', (opcao) => {
+            switch (opcao) {
+                case '1':
+                    rl.question('Digite o nome da tarefa: ', (nome) => {
+                        rl.question('Digite o status da tarefa: ', (status) => {
+                            gerenciador.adicionarTarefa(new Tarefa(nome, status));
+                            console.log('Tarefa adicionada com sucesso!');
+                            menu();
+                        });
+                    });
+                    break;
+                case '2':
+                    rl.question('Digite o nome da tarefa para remover: ', (nome) => {
+                        gerenciador.removerTarefa(nome);
+                        console.log('Tarefa removida com sucesso!');
+                        menu();
+                    });
+                    break;
+                case '3':
+                    console.log('Tarefas:');
+                    console.log(gerenciador.listasTodasTarefas());
+                    menu();
+                    break;
+                case '4':
+                    rl.question('Digite o nome da tarefa: ', (nome) => {
+                        rl.question('Digite o novo status: ', (status) => {
+                            gerenciador.alterarStatus(nome, status);
+                            console.log('Status alterado com sucesso!');
+                            menu();
+                        });
+                    });
+                    break;
+                case '5':
+                    rl.question('Digite o status para filtar: ', (status) => {
+                        console.log('tarefas filtradas: ');
+                        console.log(gerenciador.listarTarefasPorStatus(status));
+                        menu();
+                    });
+                    break;
+                case '0':
+                    console.log('Saindo...');
+                    rl.close();
+                    break;
+                default:    
+                    console.log('Opção inválida!');
+                    menu();
+                    break;
+        }
+    });    
+}
 
-const tarefas = gerenciador.listasTodasTarefas();
-const pendentes = gerenciador.listarTarefasPorStatus('pendente');
-const concluidas = gerenciador.listarTarefasPorStatus('concluído');
-const andamento = gerenciador.listarTarefasPorStatus('em andamento');
-
-// console.log("Todas as tarefas:", tarefas);
-
-// console.log("Tarefas pendentes:", gerenciador.listarTarefasPorStatus("pendente"));
-
-gerenciador.alterarStatus("Fazer exercícios", "concluído");
-
-// console.log("Todas as tarefas atualizadas:", gerenciador.listarTarefas());
-
-// gerenciador.removerTarefa("Fazer exercícios");
-
-// console.log("Tarefas restantes:", gerenciador.listarTarefas());
+menu();
 
